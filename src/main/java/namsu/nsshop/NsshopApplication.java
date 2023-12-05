32c14d5fb6fca1d5665a291d28c89767a9512370
skatn <87296704+skatn@users.noreply.github.com>
2023-12-06 03:10:16 +0900

    Security 비활성화, Jacoco 적용

--- src/main/java/namsu/nsshop/NsshopApplication.java
+++ src/main/java/namsu/nsshop/NsshopApplication.java
@@ -2,8 +2,9 @@ package namsu.nsshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NsshopApplication {
 
     public static void main(String[] args) {
--- src/test/java/namsu/nsshop/NsshopApplicationTests.java
+++ src/test/java/namsu/nsshop/NsshopApplicationTests.java
@@ -1,13 +1,24 @@
 package namsu.nsshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+
+
 class NsshopApplicationTests {
+
+    NsshopApplication application = new NsshopApplication();
 
     @Test
     void contextLoads() {
+        MockedStatic<SpringApplication> ms = Mockito.mockStatic(SpringApplication.class);
+        ms.when(() -> SpringApplication.run((Class<?>) any(), any()))
+                .thenReturn(null);
+        NsshopApplication.main(null);
+        Mockito.verify(SpringApplication.class);
     }
 
 }
