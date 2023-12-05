05e5c495a75b043d3ce59dc74517ff192ba86d02
skatn <87296704+skatn@users.noreply.github.com>
2023-12-05 23:35:10 +0900

    Initial commit

---
+++ src/main/java/namsu/nsshop/NsshopApplication.java
@@ -0,0 +1,13 @@
+package namsu.nsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NsshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/namsu/nsshop/NsshopApplicationTests.java
@@ -0,0 +1,13 @@
+package namsu.nsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
